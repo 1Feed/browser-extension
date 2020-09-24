903dfadeb3b8ee667a85e05e02875ffd6dd9f383
Ethan <me@ethan.link>
2020-09-25 08:22:48 +1000

    Added utm_source query parameter to redirected URL

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.browserAction.onClicked.addListener((tab) =>
   chrome.tabs.update(tab.id, {
-    url: "https://my.1feed.app/add?url=" + encodeURIComponent(tab.url),
+    url:
+      "https://my.1feed.app/add?url=" +
+      encodeURIComponent(tab.url) +
+      "&utm_source=extension",
   })
 );

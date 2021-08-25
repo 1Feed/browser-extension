1b6c0322ec8b0e0b29d75753fffe4692bb87d838
Ethan <me@ethan.link>
2021-08-25 20:52:58 +1000

    Open a welcome page on install, removing the need for the browser_action title to say "[sends page URL to 1Feed]"

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+// This code runs when you first install the extension, showing a welcome page
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  // See https://stackoverflow.com/a/64660664
+  if (reason === "install")
+    chrome.tabs.create({ url: "https://1feed.app/extension-installed/" });
+});
+
+// This code runs when you click the 1Feed icon in your browser
 chrome.browserAction.onClicked.addListener((tab) =>
   chrome.tabs.create({
     url:

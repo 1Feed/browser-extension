10768f6abf2d298f5bec2c8d5e82b9ee8a97fd0a
Ethan <me@ethan.link>
2021-08-30 13:40:52 +1000

    If clicked on the new tab page, open 1Feed Home in the current tab

--- background.js
+++ background.js
@@ -6,11 +6,19 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 });
 
 // This code runs when you click the 1Feed icon in your browser
-chrome.browserAction.onClicked.addListener((tab) =>
-  chrome.tabs.create({
-    url:
-      "https://my.1feed.app/add?url=" +
-      encodeURIComponent(tab.url) +
-      "&utm_source=extension",
-  })
-);
+chrome.browserAction.onClicked.addListener((tab) => {
+  if (["about:newtab", "chrome://newtab/"].includes(tab.url)) {
+    // If on the new tab page, open 1Feed in the current tab
+    chrome.tabs.update(tab.id, {
+      url: "https://my.1feed.app/?utm_source=extension",
+    });
+  } else {
+    // If on any other page, open the 1Feed add page in new tab
+    chrome.tabs.create({
+      url:
+        "https://my.1feed.app/add?url=" +
+        encodeURIComponent(tab.url) +
+        "&utm_source=extension",
+    });
+  }
+});
